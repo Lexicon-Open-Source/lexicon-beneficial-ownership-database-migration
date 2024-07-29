a739172c682f64118d8eba3f0408080d234ab1a8
Adryan Eka Vandra <adryanekavandra@gmail.com>
2024-07-29 17:42:07 +0700

    feat: alter extraction too

--- lexicon-bo-crawler/03-add-unique-constraint-url-frontier.sql
+++ lexicon-bo-crawler/03-add-unique-constraint-url-frontier.sql
@@ -2,3 +2,8 @@
 ALTER TABLE url_frontier ADD CONSTRAINT url_frontier_unique UNIQUE (url);
 
 ALTER TABLE url_frontier ALTER COLUMN id TYPE varchar(64) ;
+
+alter table url_frontier drop column hash;
+
+ALTER TABLE extraction ALTER COLUMN id TYPE varchar(64) ;
+alter table extraction alter column "url_frontier_id" type varchar(64);
