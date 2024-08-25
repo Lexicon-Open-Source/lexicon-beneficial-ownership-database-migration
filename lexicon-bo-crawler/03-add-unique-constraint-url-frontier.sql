67dd0c70c1fe1fc2b34352f34409d59bdc05cd97
Adryan Eka Vandra <adryanekavandra@gmail.com>
2024-08-25 13:30:28 +0700

    fix: update metatada

--- lexicon-bo-crawler/03-add-unique-constraint-url-frontier.sql
+++ lexicon-bo-crawler/03-add-unique-constraint-url-frontier.sql
@@ -1,9 +1,12 @@
 -- POSTGRESQL
 ALTER TABLE url_frontier ADD CONSTRAINT url_frontier_unique UNIQUE (url);
 
-ALTER TABLE url_frontier ALTER COLUMN id TYPE varchar(64) ;
+ALTER TABLE url_frontier ALTER COLUMN id TYPE VARCHAR(64) ;
 
 alter table url_frontier drop column hash;
 
-ALTER TABLE extraction ALTER COLUMN id TYPE varchar(64) ;
-alter table extraction alter column "url_frontier_id" type varchar(64);
+ALTER TABLE extraction ALTER COLUMN id TYPE VARCHAR(64) ;
+ALTER TABLE extraction ALTER column "url_frontier_id" TYPE VARCHAR(64);
+
+-- ADD Column metadata to extraction table
+ALTER TABLE extraction ADD column metadata JSONB NULL;
